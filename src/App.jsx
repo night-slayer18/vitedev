b20f6ec5b6cc63128546b281217e7cb614c3422c
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-02-16 10:46:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Product from './components/Product'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Product/>
+    </>
+  )
+}
+
+export default App
